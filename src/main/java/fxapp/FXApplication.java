23a40f0f3c83e0a2e04d7980719b404f307120c1
rlowenstein1 <rmlowenstein97@gmail.com>
2018-04-18 16:38:59 -0400

    Added functionality to New Owner / New Visitor Registration buttons

--- src/main/java/fxapp/FXApplication.java
+++ src/main/java/fxapp/FXApplication.java
@@ -10,9 +10,9 @@ public class FXApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/main/resources/view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/main/resources/view/UserLoginWindow.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
--- src/main/resources/sample/Controller.java
+++ src/main/resources/sample/Controller.java
@@ -1,4 +1,42 @@
 package main.resources.sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+
+	public void onClickLogin() {
+		//SUBMIT QUERY TO DATABASE
+	}
+
+	public void onClickNewOwnerRegistration() {
+		Parent root = null;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/main/resources/view/NewOwnerRegistration.fxml"));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		Stage stage = new Stage();
+		stage.setTitle("New Owner Registration");
+		stage.setScene(new Scene(root, 750, 600));
+		stage.show();
+	}
+
+	public void onClickNewVisitorRegistration() {
+		Parent root = null;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/main/resources/view/NewVisitorRegistration.fxml"));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		Stage stage = new Stage();
+		stage.setTitle("New Visitor Registration");
+		stage.setScene(new Scene(root, 750, 600));
+		stage.show();
+	}
 }
