db95096f89f9c176424eebf0cb01d881f4a30a90
nkstevo <nkstevo@gmail.com>
2018-04-22 21:24:39 -0400

    Visitor TableView populated

--- src/main/java/controller/VisitorDefaultViewController.java
+++ src/main/java/controller/VisitorDefaultViewController.java
@@ -10,9 +10,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import main.java.db.PropertyViewServiceImpl;
 import main.java.model.Property;
+import main.java.model.PropertyView;
 import main.java.model.User;
 
 import java.io.IOException;
@@ -43,42 +46,43 @@ public class VisitorDefaultViewController {
     private Button logOutButton;
 
     @FXML
-    private TableView<?> validProps;
+    private TableView<PropertyView> validProperties;
 
     @FXML
-    private TableColumn<?, ?> nameCol;
+    private TableColumn<PropertyView, String> nameCol;
 
     @FXML
-    private TableColumn<?, ?> addressCol;
+    private TableColumn<PropertyView, String> addressCol;
 
     @FXML
-    private TableColumn<?, ?> cityCol;
+    private TableColumn<PropertyView, String> cityCol;
 
     @FXML
-    private TableColumn<?, ?> zipCol;
+    private TableColumn<PropertyView, Integer> zipCol;
 
     @FXML
-    private TableColumn<?, ?> sizeCol;
+    private TableColumn<PropertyView, Float> sizeCol;
 
     @FXML
-    private TableColumn<?, ?> typeCol;
+    private TableColumn<PropertyView, String> typeCol;
 
     @FXML
-    private TableColumn<?, ?> publicCol;
+    private TableColumn<PropertyView, Boolean> publicCol;
 
     @FXML
-    private TableColumn<?, ?> commercialCol;
+    private TableColumn<PropertyView, Boolean> commercialCol;
 
     @FXML
-    private TableColumn<?, ?> idCol;
+    private TableColumn<PropertyView, Integer> idCol;
 
     @FXML
-    private TableColumn<?, ?> visitsCol;
+    private TableColumn<PropertyView, Integer> visitsCol;
 
     @FXML
-    private TableColumn<?, ?> avgRatingCol;
+    private TableColumn<PropertyView, Integer> avgRatingCol;
 
     public Property p = new Property(0, "Atwood Street Garden", 1.0f, false, true, "Atwood Street SW", "Atlanta",30308, Property.PropertyType.GARDEN, "gardenowner");
+    private User user;
 
 
     @FXML
@@ -120,5 +124,26 @@ public class VisitorDefaultViewController {
         stage.show();
     }
 
+    public void loadUser(User user) {
+        this.user = user;
+    }
+
+    public void loadProperties() {
+        nameCol.setCellValueFactory(new PropertyValueFactory<PropertyView, String>("name"));
+        addressCol.setCellValueFactory(new PropertyValueFactory<PropertyView, String>("street"));
+        cityCol.setCellValueFactory(new PropertyValueFactory<PropertyView, String>("city"));
+        zipCol.setCellValueFactory(new PropertyValueFactory<PropertyView, Integer>("zipcode"));
+        sizeCol.setCellValueFactory(new PropertyValueFactory<PropertyView, Float>("size"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<PropertyView, String>("propertyType"));
+        publicCol.setCellValueFactory(new PropertyValueFactory<PropertyView, Boolean>("isPublic"));
+        commercialCol.setCellValueFactory(new PropertyValueFactory<PropertyView, Boolean>("isCommercial"));
+        idCol.setCellValueFactory(new PropertyValueFactory<PropertyView, Integer>("id"));
+        visitsCol.setCellValueFactory(new PropertyValueFactory<PropertyView, Integer>("visits"));
+        avgRatingCol.setCellValueFactory(new PropertyValueFactory<PropertyView, Integer>("averageRating"));
+
+        PropertyViewServiceImpl propertyViewService = new PropertyViewServiceImpl();
+
+        validProperties.getItems().setAll(propertyViewService.findAllOtherConfirmedOrdered("Name ASC", null, null, user.getUsername()));
+    }
 }
 
